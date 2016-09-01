d65784f47ffd0157278de346b44f46c22e536e80
Michael Tucker <mictuc@stanford.edu>
2016-08-31 23:44:45 -0700

    added support for variables for easy editting

---
+++ js/variables.js
@@ -0,0 +1,64 @@
+/*
+ * This file contains the constants that will appear on the Ram's Head site.
+ * Please only edit necessary changes.
+ *
+ * Michael Tucker 8/31/16 mictuc@stanford.edu, 609-672-9724
+ * (c) Ram's Head Theatrical Society 2016
+ */
+
+// Slide One Information
+var slide_one_title = "Congrats to Theory of Relativity";
+var slide_one_subtitle = "On a Fabulous West Coast Premiere!";
+var slide_one_button = false;
+var slide_one_link = "";
+
+// Slide two Information
+var slide_two_title = "Rent";
+var slide_two_subtitle = "\"Rent reminds us to live for the moment\"";
+var slide_two_button = false;
+var slide_two_link = "";
+
+// Slide three Information
+var slide_three_title = "Hairspray";
+var slide_three_subtitle = "Drawing parallels to the civil rights movement";
+var slide_two_button = true;
+var slide_three_link = "http://news.stanford.edu/2015/04/09/hairspray-rams-head-040915/";
+
+// Gaieties Information
+var gaieties_title = "Gaieties 2016";
+var gaieties_producer_name = "Alejandra Aguilar";
+var gaieties_producer_email = "aaguilar2@stanford.edu";
+var gaieties_description = "";
+
+// OWOA Information
+var owoa_title = "Original Winter One Acts 2017";
+var owoa_producer_name = "Meley Gebresellassie";
+var owoa_producer_email = "meleyg@stanford.edu";
+var owoa_description = "";
+
+// Spring Show Information
+var spring_show_title = "The Wild Party";
+var spring_show_producer_name = "Andy Kao";
+var spring_show_producer_email = "andykao@stanford.edu";
+var spring_show_description = "";
+
+// Fourth Show Information
+var fourth_show_this_season = false;
+var fourth_show_title = "";
+var fourth_show_producer_name = "";
+var fourth_show_producer_email = "";
+var fourth_show_description = "";
+
+
+// Leadership Information
+var ep_name = "Holly Dayton";
+var ep_email = "daytonh@stanford.edu";
+var board_members = "Michael Tucker, Stephanie Sapp, James Sherwood, Alison Valentine, Victor Ragsdale, Charlotte Dubach-Reinhold";
+var alumni_coordinator = "";
+var social_manager = "";
+var financial_officer = "James Sherwood";
+var theatrical_resource_manager = "Sage Voorhees, Conor Asercion";
+var publicity_manager = "";
+var historian_name = "Holly Dayton";
+var historian_email = "daytonh@stanford.edu";
+var mailing_address = "Holly Dayton";
